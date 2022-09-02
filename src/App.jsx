9ba5ba6ca3abae4680ce0321d8edc2a7cd7ebe32
paulo.tavares <paulo.tavares@olxbr.com>
2022-09-02 20:18:34 -0300

    turning js to jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { render } from "react-dom";
+
+import Pet from "./Pet";
+const App = () => {
+  return (
+    <div>
+      <h1 id="brand">Adopt Me!</h1>
+      <Pet name="Kilindo" animal="Cat" breed="SRD" />
+      <Pet name="Nicole" animal="Cat" breed="SRD" />
+      <Pet name="Pandora" animal="Dog" breed="Pastor Alemão" />
+    </div>
+  );
+};
+render(<App/>, document.getElementById("root"));
