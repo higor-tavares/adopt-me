df6861226551b941a8970fecb71785727b1de412
paulo.tavares <paulo.tavares@olxbr.com>
2022-09-02 16:59:34 -0300

    Adding parcel bundler

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
-/* global React ReactDOM */
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
-
+import React from 'react'
+import { render } from 'react-dom'
+import Pet from './Pet'
 const App = () => {
-  return React.createElement("div", null, [
-    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
-    React.createElement(Pet, { name: "Kilindo", animal: "Cat", breed: "SRD" }),
-    React.createElement(Pet, { name: "Nicole", animal: "Cat", breed: "SRD" }),
+  return React.createElement('div', null, [
+    React.createElement('h1', { id: 'brand' }, 'Adopt Me!'),
+    React.createElement(Pet, { name: 'Kilindo', animal: 'Cat', breed: 'SRD' }),
+    React.createElement(Pet, { name: 'Nicole', animal: 'Cat', breed: 'SRD' }),
     React.createElement(Pet, {
-      name: "Pandora",
-      animal: "Dog",
-      breed: "German Shepherd",
-    }),
-  ]);
-};
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+      name: 'Pandora',
+      animal: 'Dog',
+      breed: 'German Shepherd'
+    })
+  ])
+}
+render(React.createElement(App), document.getElementById('root'))
