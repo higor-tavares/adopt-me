59e72c964d30c705fb38525639ded026ba009167
paulo.tavares <paulo.tavares@olxbr.com>
2022-09-04 14:31:10 -0300

    usign react useState hook

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,17 +1,66 @@
 import { React, useState } from "react";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const breeds = ["Siames","Persa"];
+
   return (
     <div className="search-params">
       <form>
-        <label htmlFor="locations">Location</label>
-        <input
-          id="location"
-          type="text"
-          value={location}
-          placeholder="location"
-          onChange={(e) => setLocation(e.target.value)}
-        />
+        <label htmlFor="locations">
+          Location
+          <input
+            id="location"
+            type="text"
+            value={location}
+            placeholder="location"
+            onChange={(e) => setLocation(e.target.value)}
+          />
+        </label>
+        <label htmlFor="animals">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+            onBlur={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option />
+            {ANIMALS.map((val) => 
+              <option key={val} value={val}>
+                {val}
+              </option>
+            )}
+          </select>
+        </label>
+        <label htmlFor="breeds">
+          Breed
+          <select
+            id="breeds"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            onBlur={(e) => {
+              setBreed(e.target.value);
+            }}
+          >
+            <option />
+            {breeds.map((val) => 
+              <option key={val} value={val}>
+                {val}
+              </option>
+            )}
+          </select>
+        </label>
         <button>Submit</button>
       </form>
     </div>
