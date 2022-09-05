dc610ed406d0e74b5982d0140cf5abb6e0d5b030
paulo.tavares <paulo.tavares@olxbr.com>
2022-09-05 16:55:12 -0300

    creating a custom hook

--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,12 +1,13 @@
 import { React, useState, useEffect } from "react";
 import Pet from "./Pet";
+import useBreedList from "./useBreedList";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 const SearchParams = () => {
   const [location, setLocation] = useState("");
   const [animal, setAnimal] = useState("");
   const [breed, setBreed] = useState("");
-  const breeds = ["Siames", "Persa"];
+  const [breeds] = useBreedList(animal);
   const [pets, setPets] = useState([]);
 
   useEffect(() => {
