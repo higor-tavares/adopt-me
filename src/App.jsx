04d1b9e1c714b73776bbe55c26c007cc4b2f9cfd
paulo.tavares <paulo.tavares@olxbr.com>
2022-09-15 18:04:44 -0300

    feat: adding react router 6 on the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { StrictMode } from "react";
 import { render } from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Details from "./Details";
 import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <StrictMode>
-      <div>
-        <h1 id="brand">Adopt Me!</h1>
-        <SearchParams />
-      </div>
+      <BrowserRouter>
+        <Link to="/"><h1 id="brand">Adopt Me!</h1></Link>
+        <Routes>
+          <Route path="details/:id" element={<Details/>} />
+          <Route path="/" element={<SearchParams/>} />
+        </Routes>
+      </BrowserRouter>
     </StrictMode>
   );
 };
---
+++ src/Details.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+const Details = () => {
+    const {id} = useParams();
+    return <h2>Hi! {id}</h2>
+}
+
+export default Details;
